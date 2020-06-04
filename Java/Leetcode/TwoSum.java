f0f045187f7d1944fc85b3d1bd57496d6998a146
Kevinolinn8 <kolinn@mymail.mines.edu>
2020-06-04 08:35:39 -0500

    TwoSum java and python

---
+++ Java/Leetcode/TwoSum.java
@@ -0,0 +1,46 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+*/
+
+
+
+
+package Leetcode
+
+import java.util.HashMap;
+
+-.Java;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] retArray = new int[2];
+        HashMap <Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i ++){
+            int diff = target - nums[i];
+                if (map.containsKey(diff)){
+                    retArray[0] = i;
+                    retArray[1] = map.get(diff);   
+                }
+            
+            map.put(nums[i],i);
+        }
+        
+        return retArray;
+        
+        
+        
+    }
+
+}
