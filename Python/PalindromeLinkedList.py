74dcbe73d30a13c316b16b2c4ccfe2a7ca4fd383
Kevinolinn8 <kolinn@mymail.mines.edu>
2020-08-17 10:11:35 -0600

    Using list comparison

---
+++ Python/PalindromeLinkedList.py
@@ -0,0 +1,32 @@
+"""
+
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        
+        my_list = []
+        
+        while head:
+            my_list.append(head.val)
+            head = head.next
+            
+        
+        if my_list == my_list[::-1]:
+            return True
